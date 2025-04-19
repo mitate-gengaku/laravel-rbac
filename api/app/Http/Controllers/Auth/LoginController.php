c63ce2d7df567e25398c6ea80f82219a3acfdc02
mitate-gengaku <mitategengaku@gmail.com>
2025-04-19 12:58:23 +0900

    feat: ログイン機能の実装

---
+++ api/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+
+        $user = User::query()
+            ->where('email', "=", $email)
+            ->first();
+
+        if (!$user) {
+            return response()->json(['error' => '認証に失敗しました'], 401);
+        }
+
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $token = Auth::user()->createToken('AccessToken')->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['error' => '認証に失敗しました'], 401);
+    }
+}
