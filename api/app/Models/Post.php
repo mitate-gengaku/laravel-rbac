26bcadc78226da2ae329b4327964055621a6f040
mitate-gengaku <mitategengaku@gmail.com>
2025-04-19 10:45:56 +0900

    add: Postモデルの作成

---
+++ api/app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body'
+    ];
+
+
+    /**
+     * Get the user associated with the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'user_id');
+    }
+}
