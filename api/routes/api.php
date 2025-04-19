c6d35a8e7bd03078a441e30dbf31661dbbbaa866
mitate-gengaku <mitategengaku@gmail.com>
2025-04-19 16:11:19 +0900

    add: コメントアウトしたルートを追加

--- api/routes/api.php
+++ api/routes/api.php
@@ -23,4 +23,25 @@ Route::post("/login", LoginController::class);
 Route::group(["middleware" => "auth:sanctum"], function() {
     Route::get('/user', GetAuthUserController::class);
     Route::post("/logout", LogoutController::class);
+
+    // posts
+    // Route::get("/posts", PostListController::class);
+    // Route::get("/posts/:postId", PostDetailController::class);
+    // Route::post("/posts/", PostCreateController::class);
+    // Route::patch("/posts/:postId", PostUpdateController::class);
+    // Route::delete("/posts/:postId", PostDeleteController::class);
+
+    // roles
+    // Route::get("/roles", RoleListController::class);
+    // Route::get("/roles/:rolesId", RoleDetailController::class);
+    // Route::roles("/roles/", RoleCreateController::class);
+    // Route::patch("/roles/:rolesId", RoleUpdateController::class);
+    // Route::delete("/roles/:rolesId", RoleDeleteController::class);
+
+    // permissions
+    // Route::get("/permissions", PermissionListController::class);
+    // Route::get("/permissions/:permissionsId", PermissionDetailController::class);
+    // Route::permissions("/permissions/", PermissionCreateController::class);
+    // Route::patch("/permissions/:permissionsId", PermissionUpdateController::class);
+    // Route::delete("/permissions/:rolesId", PermissionDeleteController::class);
 });
